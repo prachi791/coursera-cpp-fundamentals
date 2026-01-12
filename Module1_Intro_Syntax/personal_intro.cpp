1f03db77432de2c1e73b2dc0f9d63ba25ed24ae6
Prachi Modi <prachimoditech@gmail.com>
2026-01-12 20:12:42 +0530

    Module 1: Intro to C++ Syntax labs

--- Module1_Intro_Syntax/personal_intro.cpp
+++ Module1_Intro_Syntax/personal_intro.cpp
@@ -1,7 +1,67 @@
+/*
+* Personal Introduction Program
+* Created by: Prachi Modi
+* Date: 12/01/2026
+*
+* Description: This program displays personal information
+* about myself in a formatted way on the console.
+*
+* My Learning Goals:
+*
+* get a good grip in basics of c++.
+* get better at logical thinking and be able to make good console based projects
+* that are actually real life based and solve a real problem.
+*
+*/
 #include<iostream>
 
 int main(){
-    std::cout << "Hello, my name is prachi!" << std::endl;
-    std::cout << "Love to code,debug and apply logic to find solution!!" << std::endl;
+    // This is header
+    std::cout << "====================================" << std::endl;
+    std::cout << "       PERSONAL INTRODUCTION        " << std::endl;
+    std::cout << "====================================" << std::endl;
+
+    // My basic info
+    std::cout << "Hello, my name is Prachi." << std::endl;
+    std::cout << "I am a student of Computer Science." << std::endl;
+    std::cout << "Hobby: playing badminton" << std::endl;
+    std::cout << "Favourite programming language: C++" << std::endl;
+    
+    // Educational background section
+    std::cout << "\nEDUCATION" << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "Degree : Computer Science & Engineering" << std::endl;
+    std::cout << "College : CSPIT- charotar university of science & technology" << std::endl;
+    std::cout << "Year : 2029" << std::endl;
+    
+    // Career Goals Section
+    std::cout << "\nCAREER GOALS" << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "Short-term : Build and publish 3 small logic-based projects in 14 days" << std::endl;
+    std::cout << "Long-term  : Become a strong problem-solving C++ developer with a solid public portfolio within 12 months, starting from january on the mentioned date above" << std::endl;
+    
+    // Why taking this course
+    std::cout << "\nWHY AM I TAKING THIS COURSE" << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "- I'm taking this course to master C++ fundamentals, sharpen my problem-solving skills, and build a strong GitHub portfolio with real projects." << std::endl;
+    
+    std::cout << "- Instead of just watching tutorials, I want to learn through hands-on practice, write clean code, and understand how real programs are structured." << std::endl;
+    
+    // My favorite programming concepts
+
+    std::cout << "\nTOPICS I'M EXCITED ABOUT" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "1. Problem solving using loops & conditionals" << std::endl;
+    std::cout << "2. Writing clean and structured code" << std::endl;
+    std::cout << "3. Working with strings and text manipulation" << std::endl;
+    std::cout << "4. Building small real-world projects" << std::endl;
+    std::cout << "5. Learning Data Structures step by step" <<std::endl;
+
+    // Closing message
+    std::cout << "\nThank you for reading my introduction!" << std::endl;
+    
+    // This is footer
+     std::cout << "====================================" << std::endl;
+
     return 0;
 }
